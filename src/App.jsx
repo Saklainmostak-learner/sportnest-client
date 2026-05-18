51a46f4b124f3e45558a88e6eee228ac99d03fbd
Saklainmostak-learner <saqlainmustaksaqi1@gmail.com>
2026-05-18 16:22:13 +0600

    Initialize SportNest client project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-8xl font-bold text-red-400">This is initial web.</div>
+    </>
+  );
+}
+
+export default App;
