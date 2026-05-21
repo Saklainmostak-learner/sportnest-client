c32d5a1bd7a5c6815372c8fd9f0d56e752187ad5
Saklainmostak-learner <saqlainmustaksaqi1@gmail.com>
2026-05-21 17:07:40 +0600

    firebase connected

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,16 +1,89 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+
+import axios from "axios";
+import { auth } from "../firebase/firebase.config";
 
 export const AuthContext = createContext(null);
 
+const googleProvider = new GoogleAuthProvider();
+
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  // REGISTER
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  // LOGIN
+  const loginUser = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  // GOOGLE LOGIN
+  const googleLogin = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  // UPDATE PROFILE
+  const updateUserProfile = (name, photo) => {
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: photo,
+    });
+  };
+
+  // LOGOUT
+  const logoutUser = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
+      setUser(currentUser);
+
+      if (currentUser?.email) {
+        await axios.post(
+          `${import.meta.env.VITE_API_URL}/jwt`,
+          { email: currentUser.email },
+          { withCredentials: true }
+        );
+      } else {
+        await axios.post(
+          `${import.meta.env.VITE_API_URL}/logout`,
+          {},
+          { withCredentials: true }
+        );
+      }
+
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
 
   const authInfo = {
     user,
-    setUser,
     loading,
-    setLoading,
+    createUser,
+    loginUser,
+    googleLogin,
+    updateUserProfile,
+    logoutUser,
   };
 
   return (
